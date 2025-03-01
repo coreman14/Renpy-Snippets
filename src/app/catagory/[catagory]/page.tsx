79da1aca3b639cf9c14eebe3af532a9237deafea
Rylan Cook <rcgamer2009@hotmail.com>
2025-03-01 16:41:25 -0700

    Add auto capitalization to catagory

--- src/app/catagory/[catagory]/page.tsx
+++ src/app/catagory/[catagory]/page.tsx
@@ -4,10 +4,12 @@ import AuthorPage from "./CatagoryPage";
 
 export default async function CatagoryPageServer(props: { params: Promise<{ catagory: string }> }) {
     const params = await props.params;
-    const catagoryName = decodeURIComponent(params.catagory);
+    let catagoryName = decodeURIComponent(params.catagory);
     // const data = await browseSimpleSearch(typeof searchString === "string" ? searchString : searchString[0]);
     const data = await catagoryAdvancedSearch(catagoryName);
-
+    catagoryName = catagoryName.trim().split(" ").map((word) => {
+      return (word[0]?.toUpperCase() || " ") + word.substring(1);
+    }).join(" ");
     const userId = (await cookies()).get("userId")?.value;
     return <AuthorPage userId={userId} pageEntries={data} catagory={catagoryName} />;
 }
